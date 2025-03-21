39d3bb9914ff8fd805ffe3f453ffed0cc9036aff
Lam Bui Phuc <builam66@gmail.com>
2025-03-21 23:28:30 +0700

    query projection

---
+++ src/EFCP.API/Endpoints/QueryProjection.cs
@@ -0,0 +1,21 @@
+﻿using Carter;
+using MediatR;
+using static EFCP.Application.Practice.Queries.QueryProjection;
+
+namespace EFCP.API.Endpoints
+{
+    public class QueryProjection : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            var getSampleGroup = app.MapGroup("/sample").WithTags("Sample");
+
+            getSampleGroup.MapGet("/query-projection", async (ISender sender) =>
+            {
+                var result = await sender.Send(new QueryProjectionQuery());
+
+                return Results.Ok(result);
+            });
+        }
+    }
+}
---
+++ src/EFCP.Application/Practice/Queries/QueryProjection.cs
@@ -0,0 +1,56 @@
+﻿using EFCP.Application.Abstractions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace EFCP.Application.Practice.Queries
+{
+    public class QueryProjection
+    {
+        public record QueryProjectionQuery() : IRequest<QueryProjectionResult>;
+
+        public record QueryProjectionResult(long WithoutQueryProjection, long WithQueryProjection);
+
+        public class QueryProjectionQueryHandler(IImdbDbContext _dbContext)
+            : IRequestHandler<QueryProjectionQuery, QueryProjectionResult>
+        {
+            public async Task<QueryProjectionResult> Handle(QueryProjectionQuery query, CancellationToken cancellationToken)
+            {
+                var stopwatch = Stopwatch.StartNew();
+
+                var newSample = await _dbContext.TitleNames
+                        .Include(t => t.Attributes)
+                        .Where(t => t.Region == null)
+                        .Select(t => new
+                        {
+                            t.TitleId,
+                            t.Title,
+                            t.Region,
+                            t.Language,
+                            t.Ordinal,
+                            Attributes = t.Attributes.Select(a => new
+                            {
+                                a.Attribute1,
+                                a.Class,
+                            }),
+                        })
+                        .ToListAsync(cancellationToken);
+
+                stopwatch.Stop();
+                var newTime = stopwatch.ElapsedMilliseconds;
+
+                stopwatch.Restart();
+
+                var oldSample = await _dbContext.TitleNames
+                        .Include(t => t.Attributes)
+                        .Where(t => t.Region == null)
+                        .ToListAsync(cancellationToken);
+
+                stopwatch.Stop();
+                var oldTime = stopwatch.ElapsedMilliseconds;
+
+                return new QueryProjectionResult(oldTime, newTime);
+            }
+        }
+    }
+}
