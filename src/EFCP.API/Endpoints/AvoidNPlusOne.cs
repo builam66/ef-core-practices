c0c42b7077332af3883e3e382a7b0d3d00ec7a40
Lam Bui Phuc <builam66@gmail.com>
2025-03-22 14:26:17 +0700

    avoid n+1

---
+++ src/EFCP.API/Endpoints/AvoidNPlusOne.cs
@@ -0,0 +1,21 @@
+﻿using Carter;
+using MediatR;
+using static EFCP.Application.Practice.Queries.AvoidNPlusOne;
+
+namespace EFCP.API.Endpoints
+{
+    public class AvoidNPlusOne : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            var getSampleGroup = app.MapGroup("/sample").WithTags("Sample");
+
+            getSampleGroup.MapGet("/avoid-n-plus-one", async (ISender sender) =>
+            {
+                var result = await sender.Send(new AvoidNPlusOneQuery());
+
+                return Results.Ok(result);
+            });
+        }
+    }
+}
---
+++ src/EFCP.Application/Practice/Queries/AvoidNPlusOne.cs
@@ -0,0 +1,48 @@
+﻿using EFCP.Application.Abstractions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace EFCP.Application.Practice.Queries
+{
+    public class AvoidNPlusOne
+    {
+        public record AvoidNPlusOneQuery() : IRequest<AvoidNPlusOneResult>;
+
+        public record AvoidNPlusOneResult(long NPlusOne, long AvoidNPlusOne);
+
+        public class AvoidNPlusOneQueryHandler(IImdbDbContext _dbContext)
+            : IRequestHandler<AvoidNPlusOneQuery, AvoidNPlusOneResult>
+        {
+            public async Task<AvoidNPlusOneResult> Handle(AvoidNPlusOneQuery query, CancellationToken cancellationToken)
+            {
+                var stopwatch = Stopwatch.StartNew();
+
+                var newSample = await _dbContext.Titles
+                    .Include(t => t.TitleName)
+                    .Take(10000)
+                    .AsNoTracking()
+                    .ToListAsync(cancellationToken);
+
+                stopwatch.Stop();
+                var newTime = stopwatch.ElapsedMilliseconds;
+
+                stopwatch.Restart();
+
+                var oldSample = await _dbContext.Titles
+                    .Take(10000)
+                    .AsNoTracking()
+                    .ToListAsync(cancellationToken);
+                foreach (var sample in oldSample)
+                {
+                    var titleName = _dbContext.TitleNames.FirstOrDefault(t => t.TitleId == sample.TitleId);
+                }
+
+                stopwatch.Stop();
+                var oldTime = stopwatch.ElapsedMilliseconds;
+
+                return new AvoidNPlusOneResult(oldTime, newTime);
+            }
+        }
+    }
+}
