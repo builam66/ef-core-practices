301e26bbab003f995320e791bf1beab7f89de046
Lam Bui Phuc <builam66@gmail.com>
2025-03-22 13:11:35 +0700

    raw sql

---
+++ src/EFCP.API/Endpoints/RawSQL.cs
@@ -0,0 +1,21 @@
+﻿using Carter;
+using MediatR;
+using static EFCP.Application.Practice.Queries.RawSQL;
+
+namespace EFCP.API.Endpoints
+{
+    public class RawSQL : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            var getSampleGroup = app.MapGroup("/sample").WithTags("Sample");
+
+            getSampleGroup.MapGet("/raw-sql", async (ISender sender) =>
+            {
+                var result = await sender.Send(new RawSQLQuery());
+
+                return Results.Ok(result);
+            });
+        }
+    }
+}
---
+++ src/EFCP.Application/Practice/Queries/RawSQL.cs
@@ -0,0 +1,44 @@
+﻿using EFCP.Application.Abstractions;
+using EFCP.Domain.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace EFCP.Application.Practice.Queries
+{
+    public class RawSQL
+    {
+        public record RawSQLQuery() : IRequest<RawSQLResult>;
+
+        public record RawSQLResult(long NormalQuery, long RawSQLQuery);
+
+        public class RawSQLQueryHandler(IImdbDbContext _dbContext)
+            : IRequestHandler<RawSQLQuery, RawSQLResult>
+        {
+            public async Task<RawSQLResult> Handle(RawSQLQuery query, CancellationToken cancellationToken)
+            {
+                var stopwatch = Stopwatch.StartNew();
+
+                var newSample = await _dbContext.TitleNames
+                        .FromSql<TitleName>($"SELECT [titleId],[ordinal],[region],[language],[isOriginal],[title] FROM [imdb].[dbo].[TitleNames] WHERE [region] is NULL")
+                        .AsNoTracking()
+                        .ToListAsync(cancellationToken);
+
+                stopwatch.Stop();
+                var newTime = stopwatch.ElapsedMilliseconds;
+
+                stopwatch.Restart();
+
+                var oldSample = await _dbContext.TitleNames
+                        .Where(t => t.Region == null)
+                        .AsNoTracking()
+                        .ToListAsync(cancellationToken);
+
+                stopwatch.Stop();
+                var oldTime = stopwatch.ElapsedMilliseconds;
+
+                return new RawSQLResult(oldTime, newTime);
+            }
+        }
+    }
+}
